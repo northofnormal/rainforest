b1a08b3bbf6779a764ce73b92231fc977106ffca
Anne <northofnormal@gmail.com>
2013-09-11 16:57:48 -0400

    Added reviews models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates_presence_of :password, on: :create
 	validates_presence_of :email
+
+	has_many :reviews
+	has_many :products :through => :reviews
 end
